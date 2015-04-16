a293031fabcbf76eba8faa1404dc8ab861764493
lucoby <lucoby@gmail.com>
2015-04-16 00:48:18 -0400

    Added LpInput model

--- src/main/java/project4/login/model/lpsolver/LpInput.java
+++ src/main/java/project4/login/model/lpsolver/LpInput.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.data.annotation.Id;
 
+import project4.login.model.StudentModel;
 import project4.login.model.classroom.Course;
 
 public class LpInput {
@@ -27,4 +28,60 @@ public class LpInput {
     private Map<String, Integer> enrollmentLimit;
     
     private List<StudentModel> students;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public List<Course> getCoursesCatalog() {
+		return coursesCatalog;
+	}
+
+	public void setCoursesCatalog(List<Course> coursesCatalog) {
+		this.coursesCatalog = coursesCatalog;
+	}
+
+	public List<Course> getRequiredCourses() {
+		return requiredCourses;
+	}
+
+	public void setRequiredCourses(List<Course> requiredCourses) {
+		this.requiredCourses = requiredCourses;
+	}
+
+	public Map<String, String> getPrerequisite() {
+		return prerequisite;
+	}
+
+	public void setPrerequisite(Map<String, String> prerequisite) {
+		this.prerequisite = prerequisite;
+	}
+
+	public Map<String, String> getCorequisite() {
+		return corequisite;
+	}
+
+	public void setCorequisite(Map<String, String> corequisite) {
+		this.corequisite = corequisite;
+	}
+
+	public Map<String, Integer> getEnrollmentLimit() {
+		return enrollmentLimit;
+	}
+
+	public void setEnrollmentLimit(Map<String, Integer> enrollmentLimit) {
+		this.enrollmentLimit = enrollmentLimit;
+	}
+
+	public List<StudentModel> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<StudentModel> students) {
+		this.students = students;
+	}
 }
